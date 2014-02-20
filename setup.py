a85a96e770fb638b682f3b7efb8e6bc36b2efdbb
Izhar Firdaus <izhar@inigo-tech.com>
2014-02-21 01:31:17 +0800

    initial setup

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='KageSenshi_Org',
       version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      description='KageSenshi website',
+      author='Izhar Firdaus',
+      author_email='izhar@kagesenshi.org',
       url='http://www.python.org/sigs/distutils-sig/',
 #      install_requires=['Django>=1.3'],
+      install_requires=[
+        'morepath',
+      ]
      )
